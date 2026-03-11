1a9fce6a3c34ca5274b239fba61596e07cae636e
Mathias Gallard <mathias.gallard@epita.fr>
2026-03-11 22:52:00 +0100

    Chess - added the fen notation support Test - added test for the fen notation and cleaned unnecessary code

--- src/chess/bitboard.cc
+++ src/chess/bitboard.cc
@@ -1,5 +1,8 @@
 #include "bitboard.hh"
 
+#include <format>
+#include <stdexcept>
+
 std::pmr::string bitboard_to_string(Bitboard board)
 {
     std::pmr::string string_board = "";
@@ -24,4 +27,69 @@ std::pmr::string bitboard_to_string(Bitboard board)
 std::pmr::string board_to_string(BoardState board)
 {
     return bitboard_to_string(board.colors[0] | board.colors[1]);
+}
+
+void load_fen(BoardState& boardstate, std::string_view fen)
+{
+    for (Bitboard& board : boardstate.colors)
+    {
+        board = 0ULL;
+    }
+    for (Bitboard& board : boardstate.pieces)
+    {
+        board = 0ULL;
+    }
+
+    int digit = 7; // Rank 8 (index 7)
+    int letter = 0; // File A (index 0)
+
+    for (char c : fen)
+    {
+        if (c == ' ')
+            break;
+
+        if (c == '/')
+        {
+            digit--;
+            letter = 0;
+            continue;
+        }
+
+        if (std::isdigit(c))
+        {
+            letter += (c - '0');
+            continue;
+        }
+
+        Cases square = static_cast<Cases>(digit * 8 + letter);
+        Color color = std::isupper(c) ? Color::WHITE : Color::BLACK;
+
+        set_bit(boardstate.colors[color], square);
+
+        switch (std::tolower(c))
+        {
+        case 'p':
+            set_bit(boardstate.pieces[Pieces::PAWN], square);
+            break;
+        case 'n':
+            set_bit(boardstate.pieces[Pieces::KNIGHT], square);
+            break;
+        case 'b':
+            set_bit(boardstate.pieces[Pieces::BISHOP], square);
+            break;
+        case 'r':
+            set_bit(boardstate.pieces[Pieces::ROOK], square);
+            break;
+        case 'q':
+            set_bit(boardstate.pieces[Pieces::QUEEN], square);
+            break;
+        case 'k':
+            set_bit(boardstate.pieces[Pieces::KING], square);
+            break;
+        default:
+            throw std::invalid_argument("Invalid FEN character");
+        }
+
+        letter++;
+    }
 }
--- tests/test_bitboard.cc
+++ tests/test_bitboard.cc
@@ -2,54 +2,75 @@
 
 #include "../src/chess/bitboard.hh"
 
-TEST(BitboardTests, CheckBitCorrectly)
+TEST(BitboardTests, CheckBit)
 {
     // Set all board to 0
     Bitboard board = 0ULL;
-    EXPECT_FALSE(check_bit(board, Cases::A1))
+    EXPECT_FALSE(check_bit(board, A1))
         << "Should have return False but returned True.\n";
     board = ~0ULL;
-    EXPECT_TRUE(check_bit(board, Cases::A1))
+    EXPECT_TRUE(check_bit(board, A1))
         << "Should have return False but returned True.\n";
 }
 
-TEST(BitboardTests, SetBitCorrectly)
+TEST(BitboardTests, SetBit)
 {
     // Set all board to 0
     Bitboard board = 0ULL;
 
     // Set A1 (Bit 0)
-    set_bit(board, Cases::A1);
+    set_bit(board, A1);
     EXPECT_EQ(board, 1ULL) << "A1 should represent 1ULL.\nBoard looks like:\n"
                            << bitboard_to_string(board);
 
     // Set H8 (Bit 63)
-    set_bit(board, Cases::H8);
+    set_bit(board, H8);
     // 1ULL << 63 is the H8 bit. The + 1ULL is because A1 is still set!
     EXPECT_EQ(board, (1ULL << 63) | 1ULL)
         << "A1 and H8 should represent 1ULL.\nBoard looks like:\n"
         << bitboard_to_string(board);
 }
 
-TEST(BitboardTests, ClearBitCorrectly)
+TEST(BitboardTests, ClearBit)
 {
     // Set all board to 1
     Bitboard board = ~0ULL;
 
-    clear_bit(board, Cases::A1);
-    EXPECT_FALSE(check_bit(board, Cases::A1));
+    clear_bit(board, A1);
+    EXPECT_FALSE(check_bit(board, A1));
 }
 
-TEST(BitboardTests, ToggleBitCorrectly)
+TEST(BitboardTests, ToggleBit)
 {
     // Set all board to 1
     Bitboard board = ~0ULL;
 
-    toggle_bit(board, Cases::A1);
-    EXPECT_FALSE(check_bit(board, Cases::A1))
-        << "Failed to toggle from 1 to 0\n";
-    toggle_bit(board, Cases::A1);
-    EXPECT_TRUE(check_bit(board, Cases::A1))
-        << "Failed to toggle from 0 to 1\n";
+    toggle_bit(board, A1);
+    EXPECT_FALSE(check_bit(board, A1)) << "Failed to toggle from 1 to 0\n";
+    toggle_bit(board, A1);
+    EXPECT_TRUE(check_bit(board, A1)) << "Failed to toggle from 0 to 1\n";
     ;
+}
+
+TEST(BitboardTests, LoadFen)
+{
+    BoardState boardState;
+    boardState.colors[WHITE] = ~0ULL;
+    boardState.colors[BLACK] = ~0ULL;
+
+    load_fen(boardState, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+    EXPECT_TRUE(check_bit(boardState.colors[WHITE], E1))
+        << "Expected White but none found";
+    EXPECT_TRUE(check_bit(boardState.pieces[KING], E1))
+        << "Expected King but none found";
+
+    EXPECT_TRUE(check_bit(boardState.colors[BLACK], C8))
+        << "Expected Black but none found";
+    EXPECT_TRUE(check_bit(boardState.pieces[BISHOP], C8))
+        << "Expected Bishop but none found";
+
+    EXPECT_FALSE(check_bit(boardState.colors[WHITE], E4))
+        << "Expected none but White found";
+    EXPECT_FALSE(check_bit(boardState.colors[BLACK], E4))
+        << "Expected none but Black found";
 }
